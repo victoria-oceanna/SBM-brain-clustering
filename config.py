a381ee3c68e6ec2484ca81c87b7c95a12cbab432
victoria-oceanna <75588030+victoria-oceanna@users.noreply.github.com>
2022-07-20 17:21:35 -0500

    Update config.py

--- config.py
+++ config.py
@@ -3,6 +3,11 @@ from scipy.io import loadmat
 
 #set global variables here
 
+#set directory containing brain data
+loc = "/home/daoutidi/shared/brain_transfer/"
+
+print("Data directory:" + loc)
+
 # import matrix of runs to use for each subject
 choose_run = loadmat("/home/daoutidi/shared/runs_to_use.mat")
 
@@ -12,5 +17,9 @@ file_names = readFile("/home/daoutidi/shared/brain_transfer/list_connectomes.txt
 # set global threshold to moderate outliers
 threshold = 0.59
 
+print("Current threshold: " + str(threshold))
+
 # set global number of SBM iterations
-iterations = 10
+iterations = 2
+
+print("Number of iterations set to " +str(iterations))
